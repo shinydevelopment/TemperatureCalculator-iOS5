784c2329778ef8a2807c8ea3fabbb14f3cc00da6
Greg Spiers <greg.spiers@shinydevelopment.com>
2012-06-26 13:28:13 +0100

    Initial import.

---
+++ Template/TemperatureCalculator/ViewController.m
@@ -0,0 +1,34 @@
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (nonatomic, retain) IBOutlet UITextField *fahrenheitTextField;
+@property (nonatomic, retain) IBOutlet UILabel *celsiusLabel;
+@end
+
+@implementation ViewController
+
+@synthesize fahrenheitTextField = _fahrenheitTextField;
+@synthesize celsiusLabel = _celsiusLabel;
+
+#pragma mark Temperature conversion and formatting helpers
+- (double)celsiusFromFahrenheit:(double)fahrenheit
+{
+  // Convert a value in fahrenheit to celsius
+  return (((fahrenheit - 32) / 9) * 5);
+}
+
+- (NSString *)formattedCelsiusString:(double)celsius
+{
+  // Convert the floating point value to a string and add ºC to the end
+  return [NSString stringWithFormat:@"%.1fºC", celsius];
+}
+
+#pragma mark Actions
+- (IBAction)convertButtonTapped
+{
+  // --------------------- //
+  // INSERT YOUR CODE HERE //
+  // --------------------- //
+}
+
+@end
